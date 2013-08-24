acfa8c43d4483ed722333372ad14527a7eba3059
slampana <justin.campana@live.com>
2013-08-24 15:51:13 -0700

    First pass at LCD.  Got power and numbers to LCD.

--- thermo_clock.py
+++ thermo_clock.py
@@ -3,36 +3,42 @@ __author__ = 'Justin'
 import time
 import datetime
 from Adafruit_7Segment import SevenSegment
+from Adafruit_LEDBackpack import LEDBackpack
 
 # ===========================================================================
 # Clock Example
 # ===========================================================================
 segment = SevenSegment(address=0x70)
+backpack = LEDBackpack()
+
+backpack.setBrightness(0)
+backpack.setBlinkRate(2)
 
 print "Press CTRL+Z to exit"
 print datetime.datetime.now()
 
 # Continually update the time on a 4 char, 7-segment display
-while True:
-    now = datetime.datetime.now()
-    hour = now.hour
-    minute = now.minute
-    second = now.second
-    # Set hours
-    segment.writeDigit(0, int(hour / 10))     # Tens
-    segment.writeDigit(1, hour % 10)          # Ones
-    # Set minutes
-    segment.writeDigit(3, int(minute / 10))   # Tens
-    segment.writeDigit(4, minute % 10)        # Ones
-    # Toggle color
-    segment.setColon(second % 2)              # Toggle colon at 1Hz
-    # Wait one second
-    time.sleep(1)
-
 # while True:
-#
-#     segment.writeDigit(0, 0)     # Tens
-#     segment.writeDigit(1, 1)         # Ones
-#     segment.writeDigit(3, 3)   # Tens
-#     segment.writeDigit(4, 4)        # Ones
+#     now = datetime.datetime.now()
+#     hour = now.hour
+#     minute = now.minute
+#     second = now.second
+#     # Set hours
+#     segment.writeDigit(0, int(hour / 10))     # Tens
+#     segment.writeDigit(1, hour % 10)          # Ones
+#     # Set minutes
+#     segment.writeDigit(3, int(minute / 10))   # Tens
+#     segment.writeDigit(4, minute % 10)        # Ones
+#     # Toggle color
+#     segment.setColon(second % 2)              # Toggle colon at 1Hz
+#     # Wait one second
 #     time.sleep(1)
+
+while True:
+
+    segment.writeDigit(0, 8)
+    segment.writeDigit(1, 8)
+    segment.writeDigitRaw(2, 0x2)
+    segment.writeDigit(3, 8)
+    segment.writeDigit(4, 8)
+    time.sleep(1)
